1e0722d6e8c7c7521b0804d57e3ad92036862ce4
stephanebressani <s.bressani@bluewin.ch>
2019-10-17 08:56:00 +0200

    change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages
 setup(
     # Project
     name = 'flatlibfr',
-    version = '0.0.1.dev2',
+    version = '0.0.1.dev3',
     
     # Sources
     packages = find_packages(),
